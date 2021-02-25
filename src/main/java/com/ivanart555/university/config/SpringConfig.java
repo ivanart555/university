e9b6dd0a453dbde2d9556fbec94fbc53e3d2a002
Ivan Atrsiushyn <ivanart555@gmail.com>
2021-02-25 22:31:44 +0300

    Added spring dependencies to pom, added SpringConfig class.

---
+++ src/main/java/com/ivanart555/university/config/SpringConfig.java
@@ -0,0 +1,31 @@
+package com.ivanart555.university.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@ComponentScan("com.ivanart555.university")
+public class SpringConfig {
+    
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/university");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("1234");
+
+        return dataSource;
+    }
+    
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+}
