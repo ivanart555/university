d1215ae24915396353fcb2fc0771f99a338dd84d
Ivan Atrsiushyn <ivanart555@gmail.com>
2021-08-28 20:23:20 +0300

    Groups pagination, fixes.
    - Added view for list of Groups with pagination.
    - Fixed views for courses and classrooms.

--- src/main/java/com/ivanart555/university/services/CourseService.java
+++ src/main/java/com/ivanart555/university/services/CourseService.java
@@ -3,7 +3,6 @@ package com.ivanart555.university.services;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import com.ivanart555.university.entities.Classroom;
 import com.ivanart555.university.entities.Course;
 import com.ivanart555.university.exception.ServiceException;
 import com.ivanart555.university.services.generic.GenericService;
