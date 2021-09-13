c3b120b29c984c0bd9a51d7b0f7822957609b33f
Ivan Atrsiushyn <ivanart555@gmail.com>
2021-09-13 15:48:38 +0300

    Added CRUD to ClassroomsController

--- src/main/java/com/ivanart555/university/controllers/ClassroomsController.java
+++ src/main/java/com/ivanart555/university/controllers/ClassroomsController.java
@@ -1,6 +1,9 @@
 package com.ivanart555.university.controllers;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -8,6 +11,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,6 +25,7 @@ import com.ivanart555.university.services.ClassroomService;
 @Controller
 @RequestMapping("/classrooms")
 public class ClassroomsController {
+    private static final String REDIRECT_CLASSROOMS = "redirect:/classrooms";
     private ClassroomService classroomService;
 
     @Autowired
@@ -30,7 +38,7 @@ public class ClassroomsController {
             @RequestParam("page") Optional<Integer> page,
             @RequestParam("size") Optional<Integer> size) throws ServiceException {
         int currentPage = page.orElse(1);
-        int pageSize = size.orElse(15);
+        int pageSize = size.orElse(10);
 
         Page<Classroom> classroomPage = classroomService.findPaginated(PageRequest.of(currentPage - 1, pageSize));
 
@@ -39,7 +47,45 @@ public class ClassroomsController {
         model.addAttribute("currentPage", currentPage);
         model.addAttribute("totalPages", classroomPage.getTotalPages());
 
+        int totalPages = classroomPage.getTotalPages();
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
+
         return "classrooms/index";
     }
 
+    @GetMapping("/new")
+    public String newClassroom(@ModelAttribute("classroom") Classroom classroom) {
+        return "classrooms/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("classroom") Classroom classroom) throws ServiceException {
+        classroomService.create(classroom);
+        return REDIRECT_CLASSROOMS;
+    }
+    
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) throws ServiceException {
+        model.addAttribute("classroom", classroomService.getById(id));
+        return "classrooms/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("classroom") Classroom classroom, @PathVariable("id") int id)
+            throws ServiceException {
+        classroomService.update(classroom);
+        return REDIRECT_CLASSROOMS;
+    }
+
+
+    @RequestMapping("/{id}/delete")
+    public String delete(@PathVariable("id") int id) throws ServiceException {
+        classroomService.delete(id);
+        return REDIRECT_CLASSROOMS;
+    }
 }
