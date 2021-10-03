2a33c96f5c20b7a74e79d463e4c1d881b17ed030
Ivan Atrsiushyn <ivanart555@gmail.com>
2021-10-03 13:47:32 +0300

    Removed code smells and bugs. Moved navbar to layout.

--- src/main/java/com/ivanart555/university/config/SpringMvcDispatcherServletInitializer.java
+++ src/main/java/com/ivanart555/university/config/SpringMvcDispatcherServletInitializer.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 public class SpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return null;
+        return new Class[] { SpringConfig.class };
     }
 
     @Override
