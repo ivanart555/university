7f9da439a92f53645e95bd0490e2e1c5a1861e30
Ivan Atrsiushyn <ivanart555@gmail.com>
2021-10-09 10:13:53 +0300

    Fixed datasource look up.

--- src/main/java/com/ivanart555/university/config/SpringConfig.java
+++ src/main/java/com/ivanart555/university/config/SpringConfig.java
@@ -22,19 +22,17 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 public class SpringConfig {
     private static final Logger LOGGER = LoggerFactory.getLogger(SpringConfig.class);
-    
+
     @Bean
     public DataSource dataSource() {
-        Context context;
         DataSource dataSource = null;
         try {
-            context = new InitialContext();
-            Context initCtx  = (Context) context.lookup("java:/comp/env");
-            dataSource = (DataSource) initCtx.lookup("jdbc/localPostgreSQL");
+            Context context = new InitialContext();
+            dataSource = (DataSource) context.lookup("java:/comp/env/jdbc/localPostgreSQL");
         } catch (NamingException e) {
             LOGGER.warn("Failed to look up JNDI resource!");
         }
-        
+
         return dataSource;
     }
 
