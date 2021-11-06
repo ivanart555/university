84b32b8e0a375b3416b34fd6d96b94dff5c56371
Ivan Atrsiushyn <ivanart555@gmail.com>
2021-11-06 17:16:15 +0300

    Course - Replace JDBCTemplate with JPA.

--- src/main/java/com/ivanart555/university/dao/impl/CourseDAOImpl.java
+++ src/main/java/com/ivanart555/university/dao/impl/CourseDAOImpl.java
@@ -4,6 +4,10 @@ import static java.lang.String.format;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +18,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import com.ivanart555.university.dao.CourseDAO;
+import com.ivanart555.university.entities.Classroom;
 import com.ivanart555.university.entities.Course;
 import com.ivanart555.university.exception.DAOException;
 import com.ivanart555.university.exception.EntityNotFoundException;
@@ -21,37 +26,28 @@ import com.ivanart555.university.exception.QueryNotExecuteException;
 import com.ivanart555.university.mappers.CourseMapper;
 
 @Component
+@Transactional
 public class CourseDAOImpl implements CourseDAO {
     private static final Logger LOGGER = LoggerFactory.getLogger(CourseDAOImpl.class);
-    private final Environment env;
-    private final JdbcTemplate jdbcTemplate;
 
-    @Autowired
-    public CourseDAOImpl(JdbcTemplate jdbcTemplate, Environment env) {
-        this.jdbcTemplate = jdbcTemplate;
-        this.env = env;
-    }
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Override
     public List<Course> getAll() {
         LOGGER.debug("Trying to get all Courses.");
-        return jdbcTemplate.query(env.getProperty("sql.courses.get.all"), new CourseMapper());
+        return entityManager.createQuery("FROM Course").getResultList();
     }
 
     @Override
     public Course getById(Integer id) throws DAOException {
         LOGGER.debug("Trying to get Course by id: {}", id);
-        Course course = new Course();
-        try {
-            course = jdbcTemplate.queryForObject(env.getProperty("sql.courses.get.byId"), new Object[] { id },
-                    new CourseMapper());
-        } catch (EmptyResultDataAccessException e) {
+        Course course = entityManager.find(Course.class, id);
+        if (course == null) {
             String msg = format("Course with id '%s' not found", id);
             throw new EntityNotFoundException(msg);
-        } catch (DataAccessException e) {
-            String msg = format("Unable to get Course with ID '%s'", id);
-            throw new QueryNotExecuteException(msg);
         }
+
         LOGGER.debug("Found:'{}'", course);
 
         return course;
@@ -60,31 +56,22 @@ public class CourseDAOImpl implements CourseDAO {
     @Override
     public void delete(Integer id) {
         LOGGER.debug("Trying to delete Course by id: {}", id);
-        jdbcTemplate.update(env.getProperty("sql.courses.delete"), id);
+        Course course = entityManager.find(Course.class, id);
+
+        if (course != null) {
+            entityManager.remove(course);
+        }
     }
 
     @Override
     public void update(Course course) throws DAOException {
         LOGGER.debug("Trying to update {}", course);
-        try {
-            jdbcTemplate.update(env.getProperty("sql.courses.update"), course.getName(),
-                    course.getDescription(),
-                    course.getId());
-        } catch (DataAccessException e) {
-            String msg = format("Unable to update '%s'", course);
-            throw new QueryNotExecuteException(msg);
-        }
+        entityManager.merge(course);
     }
 
     @Override
     public void create(Course course) throws DAOException {
         LOGGER.debug("Trying to create {}", course);
-        try {
-            jdbcTemplate.update(env.getProperty("sql.courses.create"), course.getName(),
-                    course.getDescription());
-        } catch (DataAccessException e) {
-            String msg = format("Unable to create '%s'", course);
-            throw new QueryNotExecuteException(msg);
-        }
+        entityManager.persist(course);
     }
 }
--- src/main/java/com/ivanart555/university/entities/Course.java
+++ src/main/java/com/ivanart555/university/entities/Course.java
@@ -1,8 +1,24 @@
 package com.ivanart555.university.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "courses", schema="university")
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course_id")
     private int id;
+    
+    @Column(name = "course_name", unique = true)
     private String name;
+    
+    @Column(name = "course_description")
     private String description;
 
     public Course() {
